6b639efe036f05cc1296fc1f2205732e0ed422d8
Addy Osmani <addyosmani@gmail.com>
2016-10-30 13:04:23 -0700

    Add simple tests.

---
+++ test.js
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const assert = require('assert');
+const conditions = require('./');
+
+assert.equal(conditions.REGULAR_3G.download, 96000);
+assert.equal(conditions.WIFI.download, 3932160);
+assert.equal(conditions.GOOD_2G.latency, 150);
